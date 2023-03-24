c9f620094b583baf998484c21b2e1122e2648fe4
Koki Arakawa <k.arkw87@gmail.com>
2023-03-24 22:44:26 +0900

    #1 iconAtomの作成

---
+++ src/components/learnPage/atoms/icon.tsx
@@ -0,0 +1,119 @@
+import styled from 'styled-components'
+import SvgIcon from '@mui/material/SvgIcon'
+import {
+  Home,
+  Close,
+  Undo,
+  CheckBoxOutlineBlank,
+  CheckBox,
+  Person,
+  GitHub,
+  PlayArrow,
+  History,
+  Schema,
+  Dvr,
+  Settings,
+  AddCircle,
+  BorderColor,
+  ArrowBack,
+  ArrowForward,
+  KeyboardArrowDown,
+  KeyboardArrowRight,
+  ContentCopy,
+} from '@mui/icons-material'
+
+interface IconProps {
+  fontSize?: string
+  color?: string
+  width?: string
+  height?: string
+  backgroundColor?: string
+  display?: string
+}
+
+const IconComponent = styled.div<IconProps>`
+font-size: ${({ fontSize }) => fontSize}
+color: ${({ color }) => color}
+width: ${({ width }) => width}
+height: ${({ height }) => height}
+background-color: ${({ backgroundColor }) => backgroundColor}
+display: inline-block
+svg {
+    display: block;
+}
+`
+
+interface IconWrapperProps {
+  fontSize?: string
+  width?: string
+  height?: string
+  color?: string
+  backgroundColor?: string
+}
+
+const IconWapper = styled.div<IconWrapperProps>`
+  display: inline-block;
+  font-size: ${({ fontSize }) => fontSize};
+  width: ${({ width }) => width};
+  heigth: ${({ height }) => height};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ color }) => color};
+  svg {
+    display: block;
+  }
+`
+export interface IconButtonProps {
+  color?: string
+  backgroundColor?: string
+  fontSize?: string
+}
+
+function withIconStyle(Icon: typeof SvgIcon): React.ComponentType<IconButtonProps> {
+  const IconWithStyle = (props: IconButtonProps) => {
+    const { ...rest } = props
+    return (
+      <IconWapper {...rest}>
+        <Icon />
+      </IconWapper>
+    )
+  }
+  return IconWithStyle
+}
+
+export const CloseIcon = withIconStyle(Close)
+
+export const HomeIcon = withIconStyle(Home)
+
+export const BackIcon = withIconStyle(Undo)
+
+export const CheckBoxOutlineBlankIcon = withIconStyle(CheckBoxOutlineBlank)
+
+export const CheckBoxIcon = withIconStyle(CheckBox)
+
+export const PersonIcon = withIconStyle(Person)
+
+export const GitHubIcon = withIconStyle(GitHub)
+
+export const PlayIcon = withIconStyle(PlayArrow)
+
+export const HistoryIcon = withIconStyle(History)
+
+export const TemplateIcon = withIconStyle(Schema)
+
+export const BoardIcon = withIconStyle(Dvr)
+
+export const SettingIcon = withIconStyle(Settings)
+
+export const AddIcon = withIconStyle(AddCircle)
+
+export const EditIcon = withIconStyle(BorderColor)
+
+export const ArrowBackIcon = withIconStyle(ArrowBack)
+
+export const ArrowForwardIcon = withIconStyle(ArrowForward)
+
+export const ArrowDownIcon = withIconStyle(KeyboardArrowDown)
+
+export const ArrowRightIcon = withIconStyle(KeyboardArrowRight)
+
+export const ContentCopyIcon = withIconStyle(ContentCopy)
--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,27 +1,49 @@
+import {
+  AddIcon,
+  ArrowBackIcon,
+  ArrowDownIcon,
+  ArrowForwardIcon,
+  ArrowRightIcon,
+  BackIcon,
+  BoardIcon,
+  CheckBoxIcon,
+  CheckBoxOutlineBlankIcon,
+  CloseIcon,
+  ContentCopyIcon,
+  EditIcon,
+  GitHubIcon,
+  HistoryIcon,
+  HomeIcon,
+  PersonIcon,
+  PlayIcon,
+  SettingIcon,
+  TemplateIcon,
+} from '@/components/learnPage/atoms/icon'
 import Text from '@/components/learnPage/atoms/text'
 
 export default function Docs() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Text>This is docPage.</Text>
-      <Text fontColor='red'>フォントカラー</Text>
-      <Text fontSize='2em'>フォントサイズ</Text>
-      <Text backgroundColor='blue'>背景色</Text>
-      <Text display='block' textAlign='center'>
-        配置
-      </Text>
-      <Text border='2px white solid' width='10%'>
-        ボーダー
-      </Text>
-      <Text display='block' overflow='auto' fontSize='2em'>
-        はみ出し部分 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      </Text>
-      <Text backgroundColor='red' margin='30px'>
-        magin
-      </Text>
-      <Text backgroundColor='red' padding='10px'>
-        padding
-      </Text>
+      <HomeIcon />
+      <CloseIcon />
+      <BackIcon />
+      <CheckBoxIcon />
+      <CheckBoxOutlineBlankIcon />
+      <PersonIcon />
+      <GitHubIcon />
+      <PlayIcon />
+      <HistoryIcon />
+      <TemplateIcon />
+      <BoardIcon />
+      <SettingIcon />
+      <AddIcon />
+      <EditIcon />
+      <ArrowBackIcon />
+      <ArrowForwardIcon />
+      <ArrowDownIcon />
+      <ArrowRightIcon />
+      <ContentCopyIcon />
     </div>
   )
 }
