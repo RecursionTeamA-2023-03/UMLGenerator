7bdadc6e80e40f2977d7a83fc34b7ea018fb7a43
Yasuaki Mori <mori.yasuaki18@gmail.com>
2023-03-31 00:05:25 +0900

    add authentication apis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { Request } from 'express'
+import * as cookieParser from 'cookie-parser'
+import * as csurf from 'csurf'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
+  })
+  app.use(cookieParser())
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: false, // if https connection then true
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token')
+      },
+    }),
+  )
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
