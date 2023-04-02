7d351bca11cae94e37fe90ee6d3e7e4fa1ad5158
Yasuaki Mori <mori.yasuaki18@gmail.com>
2023-04-02 12:10:59 +0900

    solve circular dependency

--- backend/src/diagram/diagram.module.ts
+++ backend/src/diagram/diagram.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { DiagramService } from './diagram.service'
 import { ProjectModule } from '../project/project.module'
 import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule, ProjectModule],
+  imports: [PrismaModule, forwardRef(() => ProjectModule)],
   exports: [DiagramService],
   providers: [DiagramService],
 })
--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { PrismaModule } from '../prisma/prisma.module'
 import { ProjectController } from './project.controller'
 import { ProjectService } from './project.service'
 import { DiagramModule } from '../diagram/diagram.module'
 
 @Module({
-  imports: [PrismaModule, DiagramModule],
+  imports: [PrismaModule, forwardRef(() => DiagramModule)],
   exports: [ProjectService],
   controllers: [ProjectController],
   providers: [ProjectService],
