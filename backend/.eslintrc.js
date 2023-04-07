8a9d48fc09ff32fea09528dc7eb26457989b6ce6
Yasuaki Mori <mori.yasuaki18@gmail.com>
2023-04-07 23:00:35 +0900

    fixed build error by monorepo eslint settings

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    "../.eslintrc.json",
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
