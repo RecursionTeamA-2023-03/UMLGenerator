b2d78f0ee0cd3be874e5dcba70ba91332c371e10
sora_uzu <naruto07170717@gmail.com>
2023-04-25 13:56:12 +0900

    #78 目次リンク追加

--- frontend/src/pages/api/cheatSheets/toc.tsx
+++ frontend/src/pages/api/cheatSheets/toc.tsx
@@ -2,14 +2,51 @@ import { useEffect } from 'react'
 import tocbot from 'tocbot'
 
 export default function TopicsCard() {
+  const addIdsToTitle = () => {
+    const entryContainer = document.querySelector('.cheatSheets')
+
+    if (!entryContainer) {
+      return
+    }
+
+    const headings = entryContainer
+
+    ;[].forEach.call(headings.querySelectorAll('h2, h3'), (heading: HTMLElement) => {
+      const id = heading.textContent
+      if (!heading.getAttribute('id') && id != null) {
+        heading.setAttribute('id', id)
+      }
+    })
+  }
+
+  const isHeadingsExists = () => {
+    const entryContainer = document.querySelector('.cheatSheets')
+    if (!entryContainer) {
+      return
+    }
+    const headings = entryContainer.querySelectorAll('h2, h3')
+    if (headings.length === 0) {
+      return false
+    }
+    return true
+  }
   useEffect(() => {
-    console.log('useEffect')
     tocbot.init({
       tocSelector: '.toc',
       contentSelector: '.cheatSheets',
       headingSelector: 'h2, h3',
-      headingsOffset: 100,
     })
+
+    addIdsToTitle()
+    const item = document.querySelector('.toc') as HTMLElement
+    if (!item) {
+      return
+    }
+    if (!isHeadingsExists()) {
+      return
+    }
+    item.style.display = 'block'
+
     tocbot.destroy()
     return () => tocbot.refresh()
   }, [])
